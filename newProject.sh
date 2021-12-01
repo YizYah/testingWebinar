0fa837a5269de3ab983076988303edbc5aff1796
YizYah <yyakovson613@gmail.com>
2021-12-01 11:44:54 +0200

    updated newProject

--- newProject.sh
+++ newProject.sh
@@ -13,8 +13,11 @@ npm init ava
 npm i -D  typescript ts-node
 mkdir src
 mkdir test
+mkdir test/int
+mkdir test/unit
 
-# (b) AVA specs
+
+# (b) update JSON
 jq '.ava = {
     "files": [
       "test/**/*.test.ts"
@@ -24,9 +27,39 @@ jq '.ava = {
     ],
     "require": [
       "ts-node/register"
-    ]
+    ] 
   }' package.json | sponge package.json
 
-# (c) TypeScript specs
-TSCONFIG=https://raw.githubusercontent.com/YizYah/testingWebinar/main/tsconfig.json
-wget $TSCONFIG -O tsconfig.json
+
+jq '.scripts = {
+    "build": "tsc",
+    "commit": "git-cz",
+    "coverage": "nyc npm test",
+    "int-test": "ava --config int-tests.config.cjs",
+    "lint": "eslint \"src/**/*.ts\"",
+    "lintfix": "eslint \"src/**/*.ts\" --fix",
+    "prepack": "rm -rf lib && tsc -b",
+    "report": "nyc report --reporter=json",
+    "test": "ava",
+    "view-coverage": "nyc --extension .ts ava --forbid-only"
+  }' package.json | sponge package.json
+
+
+
+# (c) download files
+TS_CONFIG=https://raw.githubusercontent.com/YizYah/testingWebinar/main/tsconfig.json
+wget $TS_CONFIG -O tsconfig.json
+
+INT_CONFIG=https://raw.githubusercontent.com/YizYah/testingWebinar/main/int-tests.config.cjs
+wget $INT_CONFIG -O int-tests.config.cjs
+
+GIT_IGNORE=https://raw.githubusercontent.com/YizYah/testingWebinar/main/.gitignore
+wget $GIT_IGNORE -O .gitignore
+
+
+# make sure git is initialized
+git init
+git add . && git commit -m “init”
+mkdir .github
+mkdir .github/workflows
+
